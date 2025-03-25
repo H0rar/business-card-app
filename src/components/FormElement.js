64713b3d855451b65be6c036add43ab8a3f98cd5
Dušan Rohár <dusanrohar@gmail.com>
2025-03-25 12:17:59 +0100

    Added text onChange function

--- src/components/FormElement.js
+++ src/components/FormElement.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
+const textOnChange = () => {}
 const FormElement = ({ID, Label, Value}) => {
   return (
     <label>
         <strong>{Label}</strong>
-        <input type='text' name={ID} value={Value} />
+        <input type='text' name={ID} value={Value} onChange={textOnChange}/>
     </label>
   )
 }
