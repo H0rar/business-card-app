9ee44150f37443576bf00f8911b7d08e89a772fb
Dušan Rohár <dusanrohar@gmail.com>
2025-03-25 11:37:50 +0100

    Forms Created

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import FormElement from './FormElement'
 
 const Form = () => {
   return (
-    <section className='Form'>Form</section>
+    <section className='Form'>
+        <h2>Enter your details</h2>
+        <form>
+            <FormElement ID='Name' Label='Name' />
+            <FormElement ID='Designation' Label='Designation' />
+            <FormElement ID='Company' Label='Company' />
+            <FormElement ID='Address' Label='Address' />
+            <FormElement ID='City' Label='City' />
+            <FormElement ID='Email' Label='Email' />
+            <FormElement ID='Phone' Label='Phone' />
+        </form>
+    </section>
   )
 }
 
