a201f6fc61f2e01f21d3fff1a809ddb942537182
Dušan Rohár <dusanrohar@gmail.com>
2025-03-26 10:41:17 +0100

    Passed values from app tp prew

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Preview = () => {
   return (
-    <section className='Preview'>Preview</section>
+    <section className='Preview'>
+        <h2>Preview</h2>
+    </section>
   )
 }
 
